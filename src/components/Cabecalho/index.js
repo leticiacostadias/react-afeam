e3d688d3a96c8585c98a953399b37412472a490f
Letícia Costa <leticia.costadias@gmail.com>
2019-08-05 17:24:33 -0400

    atualizando arquivos de interface

--- src/components/Cabecalho/index.js
+++ src/components/Cabecalho/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react'
+import './cabecalho.css'
+// import './navMenu.css'
 
-import './cabecalho.module.css';
-
-class Cabecalho extends React.Component {
-  render() {
-    return (
-      // html, estrutura do componente
-      <header className="Cabecalho">
-        <h1>Twitelum</h1>
-
-        <a href="#">@{this.props.nomeUsuario}</a>
-        <div className="Cabecalho__nav">
-          {this.props.children}
-        </div>
-      </header>
-    );
-  }
+class Cabecalho extends Component {
+    render() {
+        return (
+            <header className="cabecalho">
+                <div className="cabecalho__container container">
+                    <h1 className="cabecalho__logo">
+                        <a href="/">Twitelum</a>
+                    </h1>
+                    { this.props.children }
+                </div>
+            </header>
+        )
+    }
 }
 
-export default Cabecalho;
+export default Cabecalho
