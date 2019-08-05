6d50816e4fe36a2047650bdca5fe74690f3e1ab5
Letícia Costa <leticia.costadias@gmail.com>
2019-08-05 15:59:46 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import Cabecalho from './Cabecalho';
+
+function App () {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Cabecalho />
+      </div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Cabecalho.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class Cabecalho extends React.Component {
+  render() {
+    return (
+      // html, estrutura do componente
+      <header>
+        <h1>Twitelum</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">
+                Home
+              </a>
+            </li>
+
+            <li>
+              <a href="#">
+                Notificações
+              </a>
+            </li>
+
+            <li>
+              <a href="#">
+                Mensagens
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Cabecalho;
