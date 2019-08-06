82304a997ba29596b66c6847a39afbd0a2f484eb
Letícia Costa <leticia.costadias@gmail.com>
2019-08-06 17:32:23 -0400

    adicionando lógica base do login

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -5,6 +5,35 @@ import Widget from '../../components/Widget'
 import './loginPage.css'
 
 class LoginPage extends Component {
+  handleLogar = (evento) => {
+    evento.preventDefault();
+
+    // pegar usuario e senha
+    const login = this.refs.login.value;
+    const senha = this.refs.senha.value;
+
+    // testar usuario e senha para logar
+    // usar a API
+    // fetch -> axios
+    fetch('http://api-twitelum.herokuapp.com/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        login,
+        senha
+      })
+    }).then((resposta) => {
+      console.log(resposta.ok);
+
+      return resposta.json();
+    }).then(data => console.log(data))
+      .catch((err) => {
+
+      });
+  }
+
   render() {
     return (
       <Fragment>
@@ -13,18 +42,21 @@ class LoginPage extends Component {
           <div className="container">
             <Widget>
               <h2 className="loginPage__title">Seja bem vindo!</h2>
-              <form className="loginPage__form" action="/">
+              <form
+                className="loginPage__form"
+                onSubmit={this.handleLogar}
+              >
                 <div className="loginPage__inputWrap">
                   <label className="loginPage__label" htmlFor="login">Login</label>
-                  <input className="loginPage__input" type="text" id="login" name="login" />
+                  <input ref="login" className="loginPage__input" type="text" id="login" name="login" />
                 </div>
                 <div className="loginPage__inputWrap">
                   <label className="loginPage__label" htmlFor="senha">Senha</label>
-                  <input className="loginPage__input" type="password" id="senha" name="senha" />
+                  <input ref="senha" className="loginPage__input" type="password" id="senha" name="senha" />
                 </div>
-                {/* <div className="loginPage__errorBox">
+                <div className="loginPage__errorBox">
                   Mensagem de erro!
-                </div> */}
+                </div>
                 <div className="loginPage__inputWrap">
                   <button className="loginPage__btnLogin" type="submit">
                     Logar
