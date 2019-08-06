2f727300e395862e55498dc2ba4d194cb96311ab
Letícia Costa <leticia.costadias@gmail.com>
2019-08-06 17:41:41 -0400

    adicionando facade

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,8 @@
 export { default as Cabecalho } from './Cabecalho';
+export { default as Dashboard } from './Dashboard';
+export { default as If } from './If';
+export { default as NavMenu } from './NavMenu';
+export { default as Notificacao } from './Notificacao';
+export { default as TrendsArea } from './TrendsArea';
 export { default as Tweet } from './Tweet';
 export { default as Widget } from './Widget';
-
-
-// export { default as Widget } from './Widget';
-// export { default as Widget } from './Widget';
-// export { default as Widget } from './Widget';
-// export { default as Widget } from './Widget';
-// export { default as Widget } from './Widget';
