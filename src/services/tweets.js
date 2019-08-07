d17247b941435c5a7d36c45d32b80e315c964a55
Letícia Costa <leticia.costadias@gmail.com>
2019-08-07 18:08:10 -0400

    adicionando assinatura base da service de curtir tweet

--- src/services/tweets.js
+++ src/services/tweets.js
@@ -23,6 +23,10 @@ const TweetService = {
 
     return fetch(`http://twitelum-api.herokuapp.com/tweets?X-AUTH-TOKEN=${token}`)
       .then(resposta => resposta.json());
+  },
+
+  curtirTweet () {
+    // POST http://twitelum-api.herokuapp.com/tweets/${id}/like?X-AUTH-TOKEN=${token}
   }
 }
 
