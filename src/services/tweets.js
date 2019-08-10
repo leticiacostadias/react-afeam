14b0644b80ee804731638c7e6e5029bfb6bca014
Letícia Costa <leticia.costadias@gmail.com>
2019-08-09 20:50:47 -0400

    usando api própria

--- src/services/tweets.js
+++ src/services/tweets.js
@@ -3,7 +3,7 @@ const TweetService = {
     const token = localStorage.getItem('token');
 
     // POST http://api-twitelum.herokuapp.com/tweets?X-AUTH-TOKEN=${}
-    return fetch(`http://twitelum-api.herokuapp.com/tweets?X-AUTH-TOKEN=${token}`, {
+    return fetch(`http://api-twitelum.herokuapp.com/tweets?X-AUTH-TOKEN=${token}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -18,15 +18,15 @@ const TweetService = {
     const token = localStorage.getItem('token');
 
     // fazendo um GET
-    return fetch(`http://twitelum-api.herokuapp.com/tweets?X-AUTH-TOKEN=${token}`)
+    return fetch(`http://api-twitelum.herokuapp.com/tweets?X-AUTH-TOKEN=${token}`)
       .then(resposta => resposta.json());
   },
 
   curtirTweet (idDoTweet) {
     const token = localStorage.getItem('token');
 
-    // POST http://twitelum-api.herokuapp.com/tweets/${id}/like?X-AUTH-TOKEN=${token}
-    return fetch(`http://twitelum-api.herokuapp.com/tweets/${idDoTweet}/like?X-AUTH-TOKEN=${token}`, {
+    // POST http://api-twitelum.herokuapp.com/tweets/${id}/like?X-AUTH-TOKEN=${token}
+    return fetch(`http://api-twitelum.herokuapp.com/tweets/${idDoTweet}/like?X-AUTH-TOKEN=${token}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -35,10 +35,10 @@ const TweetService = {
   },
 
   excluirTweet(idDoTweet) {
-    // DELETE http://twitelum-api.herokuapp.com/tweets/${id}?X-AUTH-TOKEN=${token}
+    // DELETE http://api-twitelum.herokuapp.com/tweets/${id}?X-AUTH-TOKEN=${token}
     const token = localStorage.getItem('token');
 
-    return fetch(`http://twitelum-api.herokuapp.com/tweets/${idDoTweet}?X-AUTH-TOKEN=${token}`, {
+    return fetch(`http://api-twitelum.herokuapp.com/tweets/${idDoTweet}?X-AUTH-TOKEN=${token}`, {
       method: 'DELETE'
     }).then(resposta => resposta.json());
   }
